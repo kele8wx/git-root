f15225fef6c94e4b1be38a3958c247f95ae19476
kele8wx <ping_85720@sina.com>
2015-12-07 11:01:26 +0800

    Update delete_log.sh

--- gitsh/delete_log.sh
+++ gitsh/delete_log.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-GameLog=/data/gamelog/
-
-find /home/fbsd/server_s[0-9]*/logs/ -mtime +14 -type d|xargs rm -rf
-find /data/gamelog/  -type d -mtime +14|xargs rm -rf 
+find /home/fbsd/server_s[0-9]*/logs/ -mtime +7 -type d|xargs rm -rf
+find /data/gamelog/  -type d -mtime +7|xargs rm -rf 
 find /data/log_post_tool/logs/ -mtime +7|xargs rm -f
 
