05f5ce7841ff80e0fcba3d6ae9fe23e60873750e
kele8wx <ping_85720@sina.com>
2015-12-07 13:50:38 +0800

    Update bashup.sh

--- gitsh/bashup.sh
+++ gitsh/bashup.sh
@@ -8,11 +8,3 @@ then
     echo "please use token download."
     exit 1
 fi
-
-#bash_update
-cd /usr/local/src/
-duang -N http://duang.opstack.cc:59808/tools/bash/bash_update.zip $token
-unzip bash_update.zip
-cd bash_update
-sh bashlog_install.sh
-cd ..
